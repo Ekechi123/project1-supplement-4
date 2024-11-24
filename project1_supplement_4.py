5ea6a40e4c6a36826466faae831654018124dad4
Ekechi123 <your_email@example.com>
2024-11-23 22:29:06 -0500

    Implement write_to_csv function

--- project1_supplement_4.py
+++ project1_supplement_4.py
@@ -5,3 +5,9 @@ def next_ten_numbers(start):
 def list_to_comma_string(string_list):
     return ",".join(string_list)
 
+
+def write_to_csv(filename, headers, data):
+    with open(filename, 'w') as file:
+        file.write(headers + "\n")
+        file.write(data + "\n")
+
